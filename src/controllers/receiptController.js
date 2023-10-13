ced3b6be28f0a95a4182d8012dcd6892663e987e
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2023-10-13 21:04:14 +0700

    get a receipt by case id and delete a receipt by case id receipt id

--- src/controllers/receiptController.js
+++ src/controllers/receiptController.js
@@ -1,32 +1,49 @@
-const { Receipt, Payment } = require('../models');
+const { Receipt, Payment, sequelize } = require('../models');
 const AppError = require('../utils/appError');
 
 exports.createReceiptByCaseId = async (req, res, next) => {
+  let t;
+
   try {
+    t = await sequelize.transaction();
     const { method, totalPrice } = req.body;
     const caseId = req.params.id;
     const staffId = req.user.id;
 
-    const receipt = await Receipt.findOne({ where: { caseId } });
+    const receipt = await Receipt.findOne(
+      { where: { caseId } },
+      { transaction: t }
+    );
     if (receipt) {
       throw new AppError('Receipt already exists for this case', 400);
     }
-    const newReceiptData = await Receipt.create({
-      method,
-      totalPrice,
-      staffId,
-      caseId
-    });
+    const newReceiptData = await Receipt.create(
+      {
+        method,
+        totalPrice,
+        staffId,
+        caseId
+      },
+      { transaction: t }
+    );
 
-    const updatePayment = await Payment.findAll({ where: { caseId } });
+    const updatePayment = await Payment.findAll(
+      { where: { caseId } },
+      { transaction: t }
+    );
 
     if (updatePayment && updatePayment.length > 0) {
       for (const payment of updatePayment) {
-        await payment.update({ receiptId: newReceiptData.id });
+        await payment.update(
+          { receiptId: newReceiptData.id },
+          { transaction: t }
+        );
       }
     } else {
       throw new AppError('Payment was not found', 400);
     }
+
+    await t.commit();
 
     const newReceipt = await Receipt.findOne({
       where: { id: newReceiptData.id }
@@ -34,30 +51,18 @@ exports.createReceiptByCaseId = async (req, res, next) => {
 
     res.status(201).json({ newReceipt });
   } catch (err) {
+    if (t) await t.rollback();
     next(err);
   }
 };
 
 exports.getReceiptByCaseId = async (req, res, next) => {
   try {
-    const caseId = req.params;
-    const receiptData = await Receipt.findOne({ where: { id: caseId } });
-
-    res.status(200).json({ receipt: receiptData });
-  } catch (err) {
-    next(err);
-  }
-};
-
-exports.deleteReceiptByCaseId = async (req, rex, next) => {
-  try {
-    const { caseId, receiptId } = req.params;
-
-    const caseIdNumber = parseInt(caseId, 10);
-    const receiptIdNumber = parseInt(receiptId, 10);
+    const id = req.params.id;
+    const caseIdNumber = parseInt(id, 10);
 
     const receiptData = await Receipt.findOne({
-      where: { id: receiptIdNumber }
+      where: { caseId: caseIdNumber }
     });
 
     if (!receiptData) {
@@ -70,10 +75,50 @@ exports.deleteReceiptByCaseId = async (req, rex, next) => {
       throw new AppError('No permission to delete', 403);
     }
 
-    await Receipt.destroy({ where: { id: receiptData.id } });
+    res.status(200).json({ receipt: receiptData });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteReceiptByCaseIdReceiptId = async (req, res, next) => {
+  let t;
+
+  try {
+    t = await sequelize.transaction();
+    const { caseId, receiptId } = req.params;
+
+    const caseIdNumber = parseInt(caseId, 10);
+    const receiptIdNumber = parseInt(receiptId, 10);
+
+    const receiptData = await Receipt.findOne({
+      where: { id: receiptIdNumber, caseId: caseIdNumber }
+    });
+
+    if (!receiptData) {
+      throw new AppError('Receipt was not found', 400);
+    }
+
+    if (caseIdNumber !== receiptData.caseId) {
+      console.log('caseIdNumber:', caseIdNumber);
+      console.log('receiptData.caseId:', receiptData.caseId);
+      throw new AppError('No permission to delete', 403);
+    }
+
+    await Payment.update(
+      { receiptId: null },
+      { where: { receiptId: receiptIdNumber }, transaction: t }
+    );
+    await Receipt.destroy({
+      where: { id: receiptIdNumber, caseId: caseIdNumber },
+      transaction: t
+    });
+
+    await t.commit();
 
     res.status(200).json({ message: 'Success delete' });
   } catch (err) {
+    if (t) await t.rollback();
     next(err);
   }
 };
--- src/routes/receiptRoute.js
+++ src/routes/receiptRoute.js
@@ -5,4 +5,10 @@ const router = express.Router();
 
 router.route('/cases/:id').post(receiptController.createReceiptByCaseId);
 
+router.route('/cases/:id').get(receiptController.getReceiptByCaseId);
+
+router
+  .route('/cases/:caseId/receipts/:receiptId')
+  .delete(receiptController.deleteReceiptByCaseIdReceiptId);
+
 module.exports = router;
